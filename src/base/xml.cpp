erengy/taiga

commit 9f3ebd24bc7da310ba62b61fee3c6aa69355af9c

Review XML functions

src/base/xml.cpp
@@ -16,67 +16,83 @@
 ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "file.h"
-#include "string.h"
 #include "xml.h"
 
-struct xml_string_writer: pugi::xml_writer {
-  std::string result;
+#include "file.h"
+#include "string.h"
 
-  virtual void write(const void* data, size_t size) {
-    result += std::string(static_cast<const char*>(data), size);
+XmlAttribute XmlAttr(XmlNode& node, const std::wstring_view name) {
+  auto attr = node.attribute(name.data());
+  if (!attr) {
+    attr = node.append_attribute(name.data());
   }
-};
+  return attr;
+}
 
-std::wstring XmlGetNodeAsString(pugi::xml_node node) {
+XmlNode XmlChild(XmlNode& node, const std::wstring_view name) {
+  auto child = node.child(name.data());
+  if (!child) {
+    child = node.append_child(name.data());
+  }
+  return child;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+std::wstring XmlDump(const XmlNode node) {
+  struct xml_string_writer : pugi::xml_writer {
+    std::wstring result;
+    void write(const void* data, size_t size) override {
+      result.append(static_cast<const pugi::char_t*>(data), size);
+    }
+  };
+
   xml_string_writer writer;
   node.print(writer);
 
-  return StrToWstr(writer.result);
+  return std::move(writer.result);
 }
 
-int XmlReadIntValue(pugi::xml_node& node, const wchar_t* name) {
-  return _wtoi(node.child_value(name));
+////////////////////////////////////////////////////////////////////////////////
+
+int XmlReadInt(const XmlNode& node, const std::wstring_view name) {
+  return ToInt(node.child_value(name.data()));
 }
 
-std::wstring XmlReadStrValue(pugi::xml_node& node, const wchar_t* name) {
-  return node.child_value(name);
+std::wstring XmlReadStr(const XmlNode& node, const std::wstring_view name) {
+  return node.child_value(name.data());
 }
 
-void XmlReadChildNodes(pugi::xml_node& parent_node,
-                       std::vector<std::wstring>& output,
-                       const wchar_t* name) {
-  for (auto child_node : parent_node.children(name)) {
-    output.push_back(child_node.child_value());
-  }
+////////////////////////////////////////////////////////////////////////////////
+
+void XmlWriteInt(XmlNode& node, const std::wstring_view name,
+                 const int value) {
+  node.append_child(name.data()).text().set(ToWstr(value).c_str());
 }
 
-void XmlWriteChildNodes(pugi::xml_node& parent_node,
+void XmlWriteStr(XmlNode& node, const std::wstring_view name,
+                 const std::wstring_view value, pugi::xml_node_type node_type) {
+  node.append_child(name.data())
+      .append_child(node_type).set_value(value.data());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void XmlWriteChildNodes(XmlNode& parent_node,
                         const std::vector<std::wstring>& input,
-                        const wchar_t* name,
+                        const std::wstring_view name,
                         pugi::xml_node_type node_type) {
   for (const auto& value : input) {
-    xml_node child_node = parent_node.append_child(name);
+    XmlNode child_node = parent_node.append_child(name.data());
     child_node.append_child(node_type).set_value(value.c_str());
   }
 }
 
-void XmlWriteIntValue(pugi::xml_node& node, const wchar_t* name, int value) {
-  xml_node child = node.append_child(name);
-  child.append_child(pugi::node_pcdata).set_value(ToWstr(value).c_str());
-}
+bool XmlWriteDocumentToFile(const XmlDocument& document,
+                            const std::wstring_view path) {
+  CreateFolder(GetPathOnly(std::wstring{path}));
 
-void XmlWriteStrValue(pugi::xml_node& node, const wchar_t* name,
-                      const wchar_t* value, pugi::xml_node_type node_type) {
-  xml_node child = node.append_child(name);
-  child.append_child(node_type).set_value(value);
-}
-
-bool XmlWriteDocumentToFile(const pugi::xml_document& document,
-                            const std::wstring& path) {
-  CreateFolder(GetPathOnly(path));
-
-  const pugi::char_t* indent = L"\x09";  // horizontal tab
-  unsigned int flags = pugi::format_default | pugi::format_write_bom;
-  return document.save_file(path.c_str(), indent, flags);
+  constexpr auto indent = L"\x09";  // horizontal tab
+  constexpr auto flags = pugi::format_default | pugi::format_write_bom;
+  return document.save_file(path.data(), indent, flags);
 }

project/vs2019/Taiga.vcxproj
@@ -60,7 +60,7 @@
       <Optimization>Disabled</Optimization>
       <PreprocessorDefinitions>WIN32;NOMINMAX;_WINDOWS;_WIN32_WINNT=0x0600;PUGIXML_NO_XPATH;PUGIXML_WCHAR_MODE;FMT_EXCEPTIONS=0;UTF8PROC_STATIC;CURL_STATICLIB;HTTP_ONLY;_DEBUG;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <ObjectFileName>$(IntDir)\x\x\%(RelativeDir)</ObjectFileName>
-      <AdditionalIncludeDirectories>..\..\deps\src;..\..\deps\src\curl\include;..\..\deps\src\date\include;..\..\deps\src\discord-rpc\include;..\..\deps\src\fmt\include;..\..\deps\src\json\single_include;..\..\deps\src\rapidjson\include;..\..\deps\src\semaver\include;..\..\src</AdditionalIncludeDirectories>
+      <AdditionalIncludeDirectories>..\..\deps\src;..\..\deps\src\curl\include;..\..\deps\src\date\include;..\..\deps\src\discord-rpc\include;..\..\deps\src\fmt\include;..\..\deps\src\json\single_include;..\..\deps\src\pugixml\src;..\..\deps\src\rapidjson\include;..\..\deps\src\semaver\include;..\..\src</AdditionalIncludeDirectories>
       <RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>
       <LanguageStandard>stdcpp17</LanguageStandard>
       <MultiProcessorCompilation>true</MultiProcessorCompilation>
@@ -88,7 +88,7 @@
       <StringPooling>true</StringPooling>
       <RuntimeLibrary>MultiThreaded</RuntimeLibrary>
       <ObjectFileName>$(IntDir)\x\x\%(RelativeDir)</ObjectFileName>
-      <AdditionalIncludeDirectories>..\..\deps\src;..\..\deps\src\curl\include;..\..\deps\src\date\include;..\..\deps\src\discord-rpc\include;..\..\deps\src\fmt\include;..\..\deps\src\json\single_include;..\..\deps\src\rapidjson\include;..\..\deps\src\semaver\include;..\..\src</AdditionalIncludeDirectories>
+      <AdditionalIncludeDirectories>..\..\deps\src;..\..\deps\src\curl\include;..\..\deps\src\date\include;..\..\deps\src\discord-rpc\include;..\..\deps\src\fmt\include;..\..\deps\src\json\single_include;..\..\deps\src\pugixml\src;..\..\deps\src\rapidjson\include;..\..\deps\src\semaver\include;..\..\src</AdditionalIncludeDirectories>
       <LanguageStandard>stdcpp17</LanguageStandard>
       <MultiProcessorCompilation>true</MultiProcessorCompilation>
     </ClCompile>

src/base/rss.cpp
@@ -23,7 +23,7 @@
 
 namespace rss {
 
-Channel ParseChannel(const xml_node& node) {
+Channel ParseChannel(const XmlNode& node) {
   Channel channel;
 
   channel.title = node.child_value(L"title");
@@ -33,7 +33,7 @@ Channel ParseChannel(const xml_node& node) {
   return channel;
 }
 
-Item ParseItem(const xml_node& node) {
+Item ParseItem(const XmlNode& node) {
   Item item;
 
   item.title = node.child_value(L"title");
@@ -68,7 +68,7 @@ Item ParseItem(const xml_node& node) {
   return item;
 }
 
-Feed ParseDocument(const xml_document& document) {
+Feed ParseDocument(const XmlDocument& document) {
   Feed feed;
 
   const auto channel_node = document.child(L"rss").child(L"channel");

src/base/settings.cpp
@@ -98,7 +98,7 @@ void Settings::InitializeKey(enum_t name, const wchar_t* default_value,
   }
 }
 
-std::wstring Settings::ReadValue(const xml_node& node_parent,
+std::wstring Settings::ReadValue(const XmlNode& node_parent,
                                  const std::wstring& path,
                                  const bool attribute,
                                  const std::wstring& default_value) {
@@ -107,7 +107,7 @@ std::wstring Settings::ReadValue(const xml_node& node_parent,
 
   const wchar_t* node_name = node_names.back().c_str();
 
-  xml_node current_node = node_parent;
+  auto current_node = node_parent;
   for (int i = 0; i < static_cast<int>(node_names.size()) - 1; i++) {
     current_node = current_node.child(node_names.at(i).c_str());
   }
@@ -115,17 +115,17 @@ std::wstring Settings::ReadValue(const xml_node& node_parent,
   if (attribute) {
     return current_node.attribute(node_name).as_string(default_value.c_str());
   } else {
-    return XmlReadStrValue(current_node, node_name);
+    return XmlReadStr(current_node, node_name);
   }
 }
 
-void Settings::ReadValue(const xml_node& node_parent, enum_t name) {
+void Settings::ReadValue(const XmlNode& node_parent, enum_t name) {
   Setting& item = map_[name];
   item.value = ReadValue(node_parent, item.path,
                          item.attribute, item.default_value);
 }
 
-void Settings::WriteValue(const xml_node& node_parent, enum_t name) {
+void Settings::WriteValue(const XmlNode& node_parent, enum_t name) {
   Setting& item = map_[name];
 
   std::vector<std::wstring> node_names;
@@ -133,7 +133,7 @@ void Settings::WriteValue(const xml_node& node_parent, enum_t name) {
 
   const wchar_t* node_name = node_names.back().c_str();
 
-  xml_node current_node = node_parent;
+  auto current_node = node_parent;
   for (int i = 0; i < static_cast<int>(node_names.size()) - 1; i++) {
     std::wstring child_name = node_names.at(i);
     if (!current_node.child(child_name.c_str())) {
@@ -146,7 +146,7 @@ void Settings::WriteValue(const xml_node& node_parent, enum_t name) {
   if (item.attribute) {
     current_node.append_attribute(node_name) = item.value.c_str();
   } else {
-    XmlWriteStrValue(current_node, node_name, item.value.c_str());
+    XmlWriteStr(current_node, node_name, item.value.c_str());
   }
 }
 

src/base/xml.h
@@ -19,31 +19,32 @@
 #pragma once
 
 #include <string>
+#include <string_view>
 #include <vector>
 
 #include <pugixml/src/pugixml.hpp>
 
-using pugi::xml_document;
-using pugi::xml_node;
-using pugi::xml_parse_result;
+using XmlAttribute = pugi::xml_attribute;
+using XmlDocument = pugi::xml_document;
+using XmlNode = pugi::xml_node;
 
-std::wstring XmlGetNodeAsString(pugi::xml_node node);
+XmlAttribute XmlAttr(XmlNode& node, const std::wstring_view name);
+XmlNode XmlChild(XmlNode& node, const std::wstring_view name);
 
-int XmlReadIntValue(pugi::xml_node& node, const wchar_t* name);
-std::wstring XmlReadStrValue(pugi::xml_node& node, const wchar_t* name);
+std::wstring XmlDump(const XmlNode node);
 
-void XmlReadChildNodes(pugi::xml_node& parent_node,
-                       std::vector<std::wstring>& output,
-                       const wchar_t* name);
-void XmlWriteChildNodes(pugi::xml_node& parent_node,
+int XmlReadInt(const XmlNode& node, const std::wstring_view name);
+std::wstring XmlReadStr(const XmlNode& node, const std::wstring_view name);
+
+void XmlWriteInt(XmlNode& node, const std::wstring_view name, const int value);
+void XmlWriteStr(XmlNode& node, const std::wstring_view name,
+                 const std::wstring_view value,
+                 pugi::xml_node_type node_type = pugi::node_pcdata);
+
+void XmlWriteChildNodes(XmlNode& parent_node,
                         const std::vector<std::wstring>& input,
-                        const wchar_t* name,
+                        const std::wstring_view name,
                         pugi::xml_node_type node_type = pugi::node_pcdata);
 
-void XmlWriteIntValue(pugi::xml_node& node, const wchar_t* name, int value);
-void XmlWriteStrValue(pugi::xml_node& node, const wchar_t* name,
-                      const wchar_t* value,
-                      pugi::xml_node_type node_type = pugi::node_pcdata);
-
-bool XmlWriteDocumentToFile(const pugi::xml_document& document,
-                            const std::wstring& path);
+bool XmlWriteDocumentToFile(const XmlDocument& document,
+                            const std::wstring_view path);

src/compat/anime_db.cpp
@@ -99,60 +99,60 @@ void Database::HandleListCompatibility(const std::wstring& meta_version) {
     SaveList();
 }
 
-void Database::ReadDatabaseInCompatibilityMode(xml_document& document) {
-  xml_node animedb_node = document.child(L"animedb");
+void Database::ReadDatabaseInCompatibilityMode(XmlDocument& document) {
+  auto animedb_node = document.child(L"animedb");
 
   for (auto node : animedb_node.children(L"anime")) {
-    std::wstring id = XmlReadStrValue(node, L"series_animedb_id");
+    std::wstring id = XmlReadStr(node, L"series_animedb_id");
     Item& item = items[ToInt(id)];  // Creates the item if it doesn't exist
     item.SetId(id, sync::kTaiga);
     item.SetId(id, sync::kMyAnimeList);
-    item.SetTitle(XmlReadStrValue(node, L"series_title"));
-    item.SetEnglishTitle(XmlReadStrValue(node, L"series_english"));
-    item.SetSynonyms(XmlReadStrValue(node, L"series_synonyms"));
-    item.SetType(sync::myanimelist::TranslateSeriesTypeFrom(XmlReadIntValue(node, L"series_type")));
-    item.SetEpisodeCount(XmlReadIntValue(node, L"series_episodes"));
-    item.SetAiringStatus(sync::myanimelist::TranslateSeriesStatusFrom(XmlReadIntValue(node, L"series_status")));
-    item.SetDateStart(Date(XmlReadStrValue(node, L"series_start")));
-    item.SetDateEnd(Date(XmlReadStrValue(node, L"series_end")));
-    item.SetImageUrl(XmlReadStrValue(node, L"series_image"));
-    item.SetGenres(XmlReadStrValue(node, L"genres"));
-    item.SetProducers(XmlReadStrValue(node, L"producers"));
-    item.SetScore(ToDouble(XmlReadStrValue(node, L"score")));
-    item.SetPopularity(XmlReadIntValue(node, L"popularity"));
-    item.SetSynopsis(XmlReadStrValue(node, L"synopsis"));
-    item.SetLastModified(ToTime(XmlReadStrValue(node, L"last_modified")));
+    item.SetTitle(XmlReadStr(node, L"series_title"));
+    item.SetEnglishTitle(XmlReadStr(node, L"series_english"));
+    item.SetSynonyms(XmlReadStr(node, L"series_synonyms"));
+    item.SetType(sync::myanimelist::TranslateSeriesTypeFrom(XmlReadInt(node, L"series_type")));
+    item.SetEpisodeCount(XmlReadInt(node, L"series_episodes"));
+    item.SetAiringStatus(sync::myanimelist::TranslateSeriesStatusFrom(XmlReadInt(node, L"series_status")));
+    item.SetDateStart(Date(XmlReadStr(node, L"series_start")));
+    item.SetDateEnd(Date(XmlReadStr(node, L"series_end")));
+    item.SetImageUrl(XmlReadStr(node, L"series_image"));
+    item.SetGenres(XmlReadStr(node, L"genres"));
+    item.SetProducers(XmlReadStr(node, L"producers"));
+    item.SetScore(ToDouble(XmlReadStr(node, L"score")));
+    item.SetPopularity(XmlReadInt(node, L"popularity"));
+    item.SetSynopsis(XmlReadStr(node, L"synopsis"));
+    item.SetLastModified(ToTime(XmlReadStr(node, L"last_modified")));
   }
 }
 
-void Database::ReadListInCompatibilityMode(xml_document& document) {
-  xml_node myanimelist = document.child(L"myanimelist");
+void Database::ReadListInCompatibilityMode(XmlDocument& document) {
+  auto myanimelist = document.child(L"myanimelist");
 
   for (auto node : myanimelist.children(L"anime")) {
     Item anime_item;
-    anime_item.SetId(XmlReadStrValue(node, L"series_animedb_id"), sync::kMyAnimeList);
+    anime_item.SetId(XmlReadStr(node, L"series_animedb_id"), sync::kMyAnimeList);
     anime_item.SetSource(sync::kMyAnimeList);
 
-    anime_item.SetTitle(XmlReadStrValue(node, L"series_title"));
-    anime_item.SetSynonyms(XmlReadStrValue(node, L"series_synonyms"));
-    anime_item.SetType(sync::myanimelist::TranslateSeriesTypeFrom(XmlReadIntValue(node, L"series_type")));
-    anime_item.SetEpisodeCount(XmlReadIntValue(node, L"series_episodes"));
-    anime_item.SetAiringStatus(sync::myanimelist::TranslateSeriesStatusFrom(XmlReadIntValue(node, L"series_status")));
-    anime_item.SetDateStart(XmlReadStrValue(node, L"series_start"));
-    anime_item.SetDateEnd(XmlReadStrValue(node, L"series_end"));
-    anime_item.SetImageUrl(XmlReadStrValue(node, L"series_image"));
+    anime_item.SetTitle(XmlReadStr(node, L"series_title"));
+    anime_item.SetSynonyms(XmlReadStr(node, L"series_synonyms"));
+    anime_item.SetType(sync::myanimelist::TranslateSeriesTypeFrom(XmlReadInt(node, L"series_type")));
+    anime_item.SetEpisodeCount(XmlReadInt(node, L"series_episodes"));
+    anime_item.SetAiringStatus(sync::myanimelist::TranslateSeriesStatusFrom(XmlReadInt(node, L"series_status")));
+    anime_item.SetDateStart(XmlReadStr(node, L"series_start"));
+    anime_item.SetDateEnd(XmlReadStr(node, L"series_end"));
+    anime_item.SetImageUrl(XmlReadStr(node, L"series_image"));
     anime_item.SetLastModified(0);
 
     anime_item.AddtoUserList();
-    anime_item.SetMyLastWatchedEpisode(XmlReadIntValue(node, L"my_watched_episodes"));
-    anime_item.SetMyDateStart(XmlReadStrValue(node, L"my_start_date"));
-    anime_item.SetMyDateEnd(XmlReadStrValue(node, L"my_finish_date"));
-    anime_item.SetMyScore(XmlReadIntValue(node, L"my_score"));
-    anime_item.SetMyStatus(sync::myanimelist::TranslateMyStatusFrom(XmlReadIntValue(node, L"my_status")));
-    anime_item.SetMyRewatching(XmlReadIntValue(node, L"my_rewatching"));
-    anime_item.SetMyRewatchingEp(XmlReadIntValue(node, L"my_rewatching_ep"));
-    anime_item.SetMyLastUpdated(XmlReadStrValue(node, L"my_last_updated"));
-    anime_item.SetMyTags(XmlReadStrValue(node, L"my_tags"));
+    anime_item.SetMyLastWatchedEpisode(XmlReadInt(node, L"my_watched_episodes"));
+    anime_item.SetMyDateStart(XmlReadStr(node, L"my_start_date"));
+    anime_item.SetMyDateEnd(XmlReadStr(node, L"my_finish_date"));
+    anime_item.SetMyScore(XmlReadInt(node, L"my_score"));
+    anime_item.SetMyStatus(sync::myanimelist::TranslateMyStatusFrom(XmlReadInt(node, L"my_status")));
+    anime_item.SetMyRewatching(XmlReadInt(node, L"my_rewatching"));
+    anime_item.SetMyRewatchingEp(XmlReadInt(node, L"my_rewatching_ep"));
+    anime_item.SetMyLastUpdated(XmlReadStr(node, L"my_last_updated"));
+    anime_item.SetMyTags(XmlReadStr(node, L"my_tags"));
 
     UpdateItem(anime_item);
   }

src/compat/history.cpp
@@ -49,7 +49,7 @@ void History::HandleCompatibility(const std::wstring& meta_version) {
 }
 
 void History::ReadQueueInCompatibilityMode(const pugi::xml_document& document) {
-  xml_node node_queue = document.child(L"history").child(L"queue");
+  auto node_queue = document.child(L"history").child(L"queue");
 
   for (auto item : node_queue.children(L"item")) {
     HistoryItem history_item;

src/compat/settings.cpp
@@ -31,7 +31,7 @@
 
 namespace taiga {
 
-void AppSettings::ReadLegacyValues(const xml_node& settings) {
+void AppSettings::ReadLegacyValues(const XmlNode& settings) {
   // Meta
   if (GetWstr(kMeta_Version).empty()) {
     const auto major = ReadValue(settings, L"meta/version/major", true, L"");

nosut/AniVault

commit 25e0452bea1c6f2cf2487df2d28f215e8d3ade58

Fix XML dump encoding
The bug was introduced in 9f3ebd24bc7da310ba62b61fee3c6aa69355af9c.

Fixes #899.

src/base/xml.cpp
@@ -41,16 +41,17 @@ XmlNode XmlChild(XmlNode& node, const std::wstring_view name) {
 
 std::wstring XmlDump(const XmlNode node) {
   struct xml_string_writer : pugi::xml_writer {
-    std::wstring result;
+    std::string result;
     void write(const void* data, size_t size) override {
-      result.append(static_cast<const pugi::char_t*>(data), size);
+      result.append(static_cast<const char*>(data), size);
     }
   };
 
   xml_string_writer writer;
-  node.print(writer);
+  node.print(writer, PUGIXML_TEXT("\t"), pugi::format_default,
+             pugi::encoding_utf8);
 
-  return std::move(writer.result);
+  return StrToWstr(writer.result);
 }
 
 ////////////////////////////////////////////////////////////////////////////////