nosut/AniVault

commit d6666a5ca335cd18238047060aa09fe53136916c

Simplify queue search

src/media/library/queue.cpp
@@ -238,55 +238,29 @@ bool Queue::IsQueued(int anime_id) const {
 
 QueueItem* Queue::FindItem(int anime_id, QueueSearch search_mode) {
   for (auto it = items.rbegin(); it != items.rend(); ++it) {
-    if (it->anime_id == anime_id && it->enabled) {
+    auto& item = *it; 
+    if (item.anime_id == anime_id && item.enabled) {
       switch (search_mode) {
-        // Date
         case QueueSearch::DateStart:
-          if (it->date_start)
-            return &(*it);
-          break;
+          return item.date_start ? &item : nullptr;
         case QueueSearch::DateEnd:
-          if (it->date_finish)
-            return &(*it);
-          break;
-        // Episode
+          return item.date_finish ? &item : nullptr;
         case QueueSearch::Episode:
-          if (it->episode)
-            return &(*it);
-          break;
-        // Notes
+          return item.episode ? &item : nullptr;
         case QueueSearch::Notes:
-          if (it->notes)
-            return &(*it);
-          break;
-        // Rewatched times
+          return item.notes ? &item : nullptr;
         case QueueSearch::RewatchedTimes:
-          if (it->rewatched_times)
-            return &(*it);
-          break;
-        // Rewatching
+          return item.rewatched_times ? &item : nullptr;
         case QueueSearch::Rewatching:
-          if (it->enable_rewatching)
-            return &(*it);
-          break;
-        // Score
+          return item.enable_rewatching ? &item : nullptr;
         case QueueSearch::Score:
-          if (it->score)
-            return &(*it);
-          break;
-        // Status
+          return item.score ? &item : nullptr;
         case QueueSearch::Status:
-          if (it->status)
-            return &(*it);
-          break;
-        // Tags
+          return item.status ? &item : nullptr;
         case QueueSearch::Tags:
-          if (it->tags)
-            return &(*it);
-          break;
-        // Default
+          return item.tags ? &item : nullptr;
         default:
-          return &(*it);
+          return &item;
       }
     }
   }
@@ -303,9 +277,10 @@ QueueItem* Queue::GetCurrentItem() {
 int Queue::GetItemCount() {
   int count = 0;
 
-  for (auto it = items.begin(); it != items.end(); ++it)
+  for (auto it = items.begin(); it != items.end(); ++it) {
     if (it->enabled)
-      count++;
+      ++count;
+  }
 
   return count;
 }

commit 3ad055c50c8925e522f82a90620bfb8422991376

Fix queue search
The bug was introduced in d6666a5ca335cd18238047060aa09fe53136916c.

src/media/library/queue.cpp
@@ -237,33 +237,38 @@ bool Queue::IsQueued(int anime_id) const {
 }
 
 QueueItem* Queue::FindItem(int anime_id, QueueSearch search_mode) {
+  const auto has_value = [&search_mode](const QueueItem& item) {
+    switch (search_mode) {
+      case QueueSearch::DateStart:
+        return item.date_start.has_value();
+      case QueueSearch::DateEnd:
+        return item.date_finish.has_value();
+      case QueueSearch::Episode:
+        return item.episode.has_value();
+      case QueueSearch::Notes:
+        return item.notes.has_value();
+      case QueueSearch::RewatchedTimes:
+        return item.rewatched_times.has_value();
+      case QueueSearch::Rewatching:
+        return item.enable_rewatching.has_value();
+      case QueueSearch::Score:
+        return item.score.has_value();
+      case QueueSearch::Status:
+        return item.status.has_value();
+      case QueueSearch::Tags:
+        return item.tags.has_value();
+      default:
+        return false;
+    }
+  };
+
   for (auto it = items.rbegin(); it != items.rend(); ++it) {
     auto& item = *it; 
-    if (item.anime_id == anime_id && item.enabled) {
-      switch (search_mode) {
-        case QueueSearch::DateStart:
-          return item.date_start ? &item : nullptr;
-        case QueueSearch::DateEnd:
-          return item.date_finish ? &item : nullptr;
-        case QueueSearch::Episode:
-          return item.episode ? &item : nullptr;
-        case QueueSearch::Notes:
-          return item.notes ? &item : nullptr;
-        case QueueSearch::RewatchedTimes:
-          return item.rewatched_times ? &item : nullptr;
-        case QueueSearch::Rewatching:
-          return item.enable_rewatching ? &item : nullptr;
-        case QueueSearch::Score:
-          return item.score ? &item : nullptr;
-        case QueueSearch::Status:
-          return item.status ? &item : nullptr;
-        case QueueSearch::Tags:
-          return item.tags ? &item : nullptr;
-        default:
-          return &item;
-      }
+    if (item.anime_id == anime_id && item.enabled && has_value(item)) {
+      return &item;
     }
   }
+
   return nullptr;
 }
 